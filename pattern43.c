deb82d9d5b7b1aec3941096862340d59aac1e715
ramla02 <ramla252017@gmail.com>
2024-10-13 20:02:36 +0300

    pattern no 43

---
+++ pattern43.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i, j, t;
+    
+    for (i = 1; i <= 5; i++)
+    {
+        t = i - 1;
+        for (j = 1; j <= i; j++)
+        {
+            printf("%3d", t + i);
+            t += 2;
+        }
+        printf("\n");
+    }
+    return (0);
+}
