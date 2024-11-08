e33f53ccca9fe0319895566ebc8f89779c3585c9
ramla02 <ramla252017@gmail.com>
2024-11-08 12:15:48 +0300

    pattern no 206

---
+++ pattern206.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n = 5;
+    int i, j;
+    
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i <= n / 2)
+            printf("0  ");
+            else
+            printf("1  ");
+        }
+        printf("\n");
+    }
+
+    return (0);
+}
