dbf82f58582ab52129ab6cb655035c67995fb31c
ramla02 <ramla252017@gmail.com>
2024-11-09 10:27:07 +0300

    pattern no 207

---
+++ pattern207.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n = 5;
+    int i, j;
+    
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (j <= i)
+            printf("1  ");
+            else
+            printf("0  ");
+        }
+        printf("\n");
+    }
+
+    return (0);
+}
